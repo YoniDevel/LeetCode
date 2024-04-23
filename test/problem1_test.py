ab4622898676570de8879ea8920d508c119cbf68
Yehonatan Moreno <jonimoreno2004@gmail.com>
2024-04-23 10:52:32 +0300

    Initialize project and solve problem 1 with tests

---
+++ test/problem1_test.py
@@ -0,0 +1,16 @@
+import pytest;
+
+from problems import problem1
+
+def testProblem1():
+    sol = problem1.Solution1();
+    twoSum = sol.twoSum;
+    betterTwoSum = sol.BetterTwoSum;
+    
+    assert twoSum([2, 7, 11, 15], 9) == [0, 1];
+    assert twoSum([3, 2, 4], 6) == [1, 2];
+    assert twoSum([3, 3], 6) == [0, 1];
+    
+    assert betterTwoSum([2, 7, 11, 15], 9) == [0, 1];
+    assert betterTwoSum([3, 2, 4], 6) == [1, 2];
+    assert betterTwoSum([3, 3], 6) == [0, 1];
