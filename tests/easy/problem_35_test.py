6bce3e8554894d4b430e4806e5f180fd072108d1
Yehonatan Moreno <jonimoreno2004@gmail.com>
2024-06-15 12:11:06 +0300

    Solve problem 35

---
+++ tests/easy/problem_35_test.py
@@ -0,0 +1,14 @@
+from src.problems.easy import problem_35;
+
+def testProblem1():
+    sol = problem_35.Solution();
+    searchInsert = sol.searchInsert;
+    betterSearchInsert = sol.betterSearchInsert;
+    
+    assert(searchInsert([1, 3, 5, 6], 5)) == 2;
+    assert(searchInsert([1, 3, 5, 6], 2)) == 1;
+    assert(searchInsert([1, 3, 5, 6], 7)) == 4;
+    
+    assert(betterSearchInsert([1, 3, 5, 6], 5)) == 2;
+    assert(betterSearchInsert([1, 3, 5, 6], 2)) == 1;
+    assert(betterSearchInsert([1, 3, 5, 6], 7)) == 4; 
